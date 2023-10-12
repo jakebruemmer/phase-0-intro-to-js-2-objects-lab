324ed334654607e15b9607b5dc24e827e05fac94
Jake Bruemmer <jakebruemmer@gmail.com>
2023-10-12 11:50:56 -0600

    Got all tests passing.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+let employee = {
+  name: 'Jake Bruemmer',
+  streetAddress: '1600 Pennsylvania Avenue'
+}
+
+let updateEmployeeWithKeyAndValue = function (employee, key, value) {
+  let newObj = {};
+  newObj[key] = value;
+  return { ...employee, ...newObj };
+}
+
+let destructivelyUpdateEmployeeWithKeyAndValue = function (employee, key, value) {
+  employee[key] = value;
+  return employee;
+}
+
+let deleteFromEmployeeByKey = function (employee, key) {
+  let newObj = { ...employee };
+  delete newObj[key];
+  return newObj;
+}
+
+let destructivelyDeleteFromEmployeeByKey = function (employee, key) {
+  delete employee[key];
+  return employee;
+}
